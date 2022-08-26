15aa24012a76c46e3c422a9d88ec5bdd7ed4fceb
Andres Celis <andres.celis@correounivalle.edu.co>
2022-08-26 02:14:56 -0500

    Styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as Logo } from "./../assets/logo.svg";
 import {
@@ -10,67 +10,87 @@ import {
 import { useNavigate } from "react-router-dom";
 const Navbar = ({ setShowCart }) => {
   const navigate = useNavigate();
+  const [position, setPosition] = useState(1);
+  const handleClick = (route, position) => {
+    navigate(route);
+    setPosition(position);
+  };
   return (
     <Nav>
       <Logo />
       <Menu>
-        <FaBars
-          onClick={() => navigate("/")}
-          className="clickeable"
-          size={20}
-        />
-        <FaArrowRight
-          onClick={() => navigate("/history")}
-          className="clickeable"
-          size={20}
-        />
-        <FaChartLine
-          onClick={() => navigate("/statistics")}
-          className="clickeable"
-          size={20}
-        />
+        <Line position={position} />
+        <IconContainer onClick={() => handleClick("/", 1)}>
+          <FaBars size={20} />
+        </IconContainer>
+        <IconContainer onClick={() => handleClick("/history", 2)}>
+          <FaArrowRight size={20} />
+        </IconContainer>
+        <IconContainer onClick={() => handleClick("/statistics", 3)}>
+          <FaChartLine size={20} />
+        </IconContainer>
       </Menu>
-      <div
-        onClick={() => setShowCart((current) => !current)}
-        className="circle"
-      >
+      <Circle onClick={() => setShowCart((current) => !current)}>
         <FaCartArrowDown color="white" size={20} />
-      </div>
+      </Circle>
     </Nav>
   );
 };
 
+const Circle = styled.div`
+  display: grid;
+  place-content: center;
+  width: 42px;
+  height: 42px;
+  background: var(--color-primary);
+  border-radius: 50%;
+  z-index: 10;
+`;
+const Line = styled.div`
+  position: absolute;
+  left: 0px;
+  top: ${(props) =>
+    props.position == 1
+      ? "0px"
+      : props.position == 2
+      ? "calc(60% - 45px)"
+      : "calc(100% - 45px)"};
+  transition: top 0.3s ease;
+  width: 6px;
+  height: 45px;
+  background: var(--color-primary);
+  border-radius: 0px 4px 4px 0px;
+`;
+const IconContainer = styled.div`
+  width: 100%;
+  height: 40px;
+  display: grid;
+  place-content: center;
+  cursor: pointer;
+`;
 const Nav = styled.nav`
   grid-area: nav;
   display: flex;
   position: fixed;
-  height: 100%;
-  padding: 30px 26px;
+  height: 100vh;
+  width: 42px;
+  padding-block: 26px;
+  z-index: 1;
   @media only screen and (min-width: 800px) {
     width: 80px;
   }
-  .clickeable {
-    cursor: pointer;
-  }
-  width: 42px;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
   background-color: white;
-  .circle {
-    display: grid;
-    place-content: center;
-    width: 42px;
-    height: 42px;
-    background: #f9a109;
-    border-radius: 50%;
-    z-index: 10;
-  }
 `;
 
 const Menu = styled.section`
   display: flex;
   flex-direction: column;
-  gap: 75.5px;
+  align-items: center;
+  gap: 60px;
+  width: 100%;
+  position: relative;
 `;
 export default Navbar;
