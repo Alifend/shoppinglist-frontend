a425cd18dce533e926e352acff6cdd078a804030
Andres Celis <andres.celis@correounivalle.edu.co>
2022-08-26 02:11:29 -0500

    View item component

--- src/components/SideContent/ViewItem/ViewItem.js
+++ src/components/SideContent/ViewItem/ViewItem.js
@@ -4,9 +4,9 @@ import { useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import { useQuery } from "@tanstack/react-query";
-import ItemServices from "../services/ItemServices";
-import { addItem, deleteItem } from "../store/ItemSlice";
-import useDeleteItem from "../hooks/useDeleteItem";
+import ItemServices from "../../../services/ItemServices";
+import { addItem, deleteItem } from "../../../store/ItemSlice";
+import { useDeleteItem } from "../../../hooks/useItem";
 const ViewItem = ({ show }) => {
   let { itemId } = useParams();
   const { isLoading, error, data } = useQuery([itemId], () =>
@@ -32,7 +32,6 @@ const ViewItem = ({ show }) => {
         <BackButton onClick={() => navigate("/")}>
           <FaLongArrowAltLeft color="#f9a109" />
           <Text color="#f9a109" fontWeight="700" fontSize="0.875rem">
-            {" "}
             back
           </Text>
         </BackButton>
@@ -91,7 +90,7 @@ const Img = styled.img`
   margin-bottom: 53px;
 `;
 const BackButton = styled.button`
-  color: #f9a109;
+  color: var(--color-primary);
   background-color: inherit;
   border: none;
   display: flex;
@@ -128,7 +127,7 @@ const Text = styled.label`
 
   width: 100%;
   &:focus-within {
-    color: #f9a109 !important;
+    color: var(--color-primary) !important;
   }
 `;
 
@@ -139,14 +138,14 @@ const Button = styled.button`
   padding: 20px 20px;
   font-weight: 700;
   border: none;
-  font-size: 16px;
+  font-size: var(--font-size-md);
   line-height: 18px;
   width: 123px;
 `;
 
 const ShoppingCartStyled = styled.aside`
   height: 100%;
-  width: calc(100% - 42px);
+  width: calc(100% - 52px);
   padding: 16.29px;
   display: flex;
   align-items: center;
@@ -155,10 +154,10 @@ const ShoppingCartStyled = styled.aside`
   /* width: clamp(320px, 11.69vw + 220.57px, calc(100vw - 42px)); */
   background: white;
   position: fixed;
-  left: ${(props) => (props.show ? "42px" : "-100%")};
+  left: ${(props) => (props.show ? "52px" : "-100%")};
   transition: left 0.4s ease;
   @media only screen and (min-width: 800px) {
-    width: 450px;
+    width: 389px;
     right: 0px;
     left: auto;
     grid-area: side;
