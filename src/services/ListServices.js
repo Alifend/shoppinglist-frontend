96a9153f1e7c44718b353cb8802e409c6e4e4434
Andres Celis <andres.celis@correounivalle.edu.co>
2022-08-26 01:31:26 -0500

    Routes of ListServices

--- src/services/ListServices.js
+++ src/services/ListServices.js
@@ -2,7 +2,19 @@ import axios from "axios";
 const API = "http://192.168.0.18:3001";
 class ListServices {
   createList(list) {
-    return axios.post(API + "/shopList/", item);
+    return axios.post(API + "/shopList/", list);
+  }
+  getLists() {
+    return axios.get(API + "/shopList/");
+  }
+  getTopSaleByField(field) {
+    return axios.get(API + "/shopList/topItemsByField/" + field);
+  }
+  getTopSelledByMonth() {
+    return axios.get(API + "/shopList/topProductsByMonth");
+  }
+  getSingleList(id) {
+    return axios.get(API + "/shopList/" + id);
   }
 }
 
